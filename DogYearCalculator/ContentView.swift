174a8ce8880c8bdc22a3ec9624cb788894f1410c
Mansib23 <mansibahmed0@gmail.com>
2025-02-20 10:55:10 +1100

    Initial Commit

---
+++ DogYearCalculator/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DogYearCalculator
+//
+//  Created by Mansib Ahmed on 20/2/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DogYearCalculator/DogYearCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  DogYearCalculatorApp.swift
+//  DogYearCalculator
+//
+//  Created by Mansib Ahmed on 20/2/2025.
+//
+
+import SwiftUI
+
+@main
+struct DogYearCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
