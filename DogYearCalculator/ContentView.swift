76514133a5ffd25396f13bbd4d41a0d66ae70e5e
Mansib23 <mansibahmed0@gmail.com>
2025-02-20 11:39:37 +1100

    hi mansib fuck you - your love aryan

--- DogYearCalculator/ContentView.swift
+++ DogYearCalculator/ContentView.swift
@@ -8,12 +8,46 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var age = 0.0
+    
+    func calculateDogAge() -> Double {
+    
+        return age*7
+    }
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            
+            
+            switch age {
+                case 0...15:
+                Text("🐶")
+                    .font(  .system(size: 72, weight: .bold, design: .default))
+            case 16...25:
+            Text("😇")
+                    .font(  .system(size: 72, weight: .bold, design: .default))
+                
+            default:
+                Text("😱")
+                    .font(  .system(size: 72, weight: .bold, design: .default))
+            }
+                
+            Text("Dog Year Calculator!")
+                .font(.title)
+                .fontDesign(.rounded)
+                .fontWeight(.bold)
+            
+            Slider(value: $age, in: 0...25)
+            
+            Text("My Age : \(Int(age)) years old")
+                .font(.title2)
+                
+            
+            Text("Dog Age \(Int(calculateDogAge())) years old")
+                .font(.title2)
+            
+            
+
         }
         .padding()
     }
